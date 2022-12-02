84fde2879a06518950553a8627f6e696d6749dac
athouba99 <athooba99@gmail.com>
2022-12-02 13:32:55 +0300

    signup and signin with their provider is done. Also, managing the routes for them

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,21 @@ import 'package:letseat/pages/signin.dart';
 
 import 'package:letseat/pages/home_page.dart';
 import 'package:letseat/providers/main_provider.dart';
+import 'package:letseat/providers/sign_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  var signProvider = SignProvider();
+  var authorized = await signProvider.tokena(); // checking the token
+
+  print("authorized $authorized"); // to test
+
+  runApp(MyApp(
+    signProvider: signProvider,
+    intitialRoute: authorized
+        ? "/"
+        : "/signin", // #### for now the other path is signin page
+  ));
 }
 
 final router = GoRouter(routes: [
@@ -18,23 +29,42 @@ final router = GoRouter(routes: [
     builder: (context, state) => HomePage(),
   ),
   GoRoute(
-    path: "/signin",
-    builder: (context, state) => Singin(),
-  ),
-  GoRoute(
     path: "/signup",
     builder: (context, state) => SignupPage(),
+  ),
+  GoRoute(
+    path: "/signin",
+    builder: (context, state) => Singin(),
   ),
 ]);
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  final String intitialRoute;
+  final SignProvider signProvider;
 
+  MyApp({required this.intitialRoute, required this.signProvider});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final router = GoRouter(initialLocation: intitialRoute, routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => HomePage(),
+      ),
+      GoRoute(
+        path: "/signup",
+        builder: (context, state) => SignupPage(),
+      ),
+      GoRoute(
+        path: "/signin",
+        builder: (context, state) => Singin(),
+      ),
+    ]);
     return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (context) => MainProvider())],
+      providers: [
+        ChangeNotifierProvider(create: (context) => MainProvider()),
+        ChangeNotifierProvider(create: (context) => signProvider),
+      ],
       child: MaterialApp.router(
         routerConfig: router,
       ),

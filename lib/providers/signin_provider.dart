d3870a00dc991a549a91fce2a9fb8d4662162661
athouba99 <athooba99@gmail.com>
2022-12-04 21:33:11 +0300

    saving progress

---
+++ lib/providers/signin_provider.dart
@@ -0,0 +1,62 @@
+// making provider for signin
+
+import 'package:flutter/cupertino.dart';
+import 'package:dio/dio.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main_provider.dart';
+
+class SigninProvider extends ChangeNotifier {
+  String? username; // to check if the user is loged in or not
+
+  // make the function future to await it & convert itstype to give reponse
+  Future<bool> sign(
+      {required String username, required String password}) async {
+    try {
+      print("HELLO HELLO");
+      // check for errors
+      var response = await MainProvider.dioClient.post("/login/", //
+          data: {
+            "username": username,
+            "password": password,
+          }); //dio client
+
+      print('******************');
+      print(response);
+      print('******************');
+      var token =
+          response.data["token"]; // the other name for the token is access
+
+      MainProvider.dioClient.options.headers["authorization"] =
+          "Bearer $token"; // setting the header for the token
+
+      this.username = username;
+
+      var preferences = await SharedPreferences.getInstance();
+      await preferences.setString("token", token); // saving the token
+
+      return true; // when the user sign successflly
+    } on DioError catch (e) {
+      print(e);
+    } catch (e) {
+      print(e);
+    }
+    return false; // when the user can't sign
+  }
+
+  Future<bool> tokena() async {
+    // check if the token exist, expired
+    var preferences = await SharedPreferences.getInstance();
+    var token = preferences.getString("tonken");
+
+    if (token != null && JwtDecoder.isExpired(token)) {
+      var tokenMap = JwtDecoder.decode(token); // converting the token to map
+      username = tokenMap["username"];
+
+      return true;
+    }
+
+    return false;
+  }
+}

0e1947d179efb4a360ad50546a2af132963cb0e7
athouba99 <athooba99@gmail.com>
2022-12-05 12:44:17 +0300

    signin is done. the only this left is a signout button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:go_router/go_router.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:letseat/providers/category_provider.dart';
 import 'package:provider/provider.dart';
 
